e978d742abfbf49b452498f6c6b916b810ddc096
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-05-13 09:18:57 +0700

    clone from hulotoy cms

---
+++ Utilities/Contants/ProductV2/ProductStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Contants.ProductV2
+{
+    public enum ProductStatus
+    {
+        ACTIVE=1,
+        DEACTIVE=2,//ẩn fe
+        REMOVE=3, // ẩn fe vs cms
+        ON_WAITING_CONFIRM = 4 // chờ duyệt
+
+    }
+}
