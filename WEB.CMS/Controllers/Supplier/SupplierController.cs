2ac81b41d233e6658c797c5b00d8c20436d095f3
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-05-29 15:12:40 +0700

    fix update user supplier

--- WEB.CMS/Controllers/Supplier/SupplierController.cs
+++ WEB.CMS/Controllers/Supplier/SupplierController.cs
@@ -668,14 +668,17 @@ namespace WEB.CMS.Controllers
                         message = "Dữ liệu không chính xác, vui lòng thử lại"
                     });
                 }
-                var exists_user = await _userRepository.GetByUserName("ncc" + request.SupplierId + "." + request.UserName);
-                if (exists_user != null && exists_user.Id > 0)
+                if (request.Id <= 0)
                 {
-                    return new JsonResult(new
+                    var exists_user = await _userRepository.GetByUserName("ncc" + request.SupplierId + "." + request.UserName);
+                    if (exists_user != null && exists_user.Id > 0)
                     {
-                        isSuccess = false,
-                        message = "Tên đăng nhập đã tồn tại, vui lòng sử dụng tên đăng nhập khác"
-                    });
+                        return new JsonResult(new
+                        {
+                            isSuccess = false,
+                            message = "Tên đăng nhập đã tồn tại, vui lòng sử dụng tên đăng nhập khác"
+                        });
+                    }
                 }
                 var suplier = _supplierRepository.GetById((int)request.SupplierId);
                 if (suplier == null || suplier.SupplierId <= 0)
