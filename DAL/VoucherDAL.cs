396ee9a7da3f5209cdd64e079594003252446ff8
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-08-22 11:44:57 +0700

    Update VoucherDAL.cs

--- DAL/VoucherDAL.cs
+++ DAL/VoucherDAL.cs
@@ -101,7 +101,6 @@ namespace DAL
                     new SqlParameter("@page_index", pageIndex),
                     new SqlParameter("@page_size", pageSize),
                     new SqlParameter("@client_id", client_id??(object)DBNull.Value),
-                    new SqlParameter("@limit_use", (object)DBNull.Value),
                 ];
                 return _DbWorker.GetDataTable("SP_GetListVoucher", objParam);
             }
